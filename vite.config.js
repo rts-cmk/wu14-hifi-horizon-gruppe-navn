b689bf1b52c02a482a26b6f316109c835a2e15eb
Lukuv <defaulttwich@gmail.com>
2025-12-19 22:42:01 +0100

    Nu vil jeg gerne i seng

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "./dist", // output mappe ændret til build
+    outDir: "build", // output mappe ændret til build
   },
 });
